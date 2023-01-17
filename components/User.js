76b815f1a0649837da13f52b9c67b93bb3acd52a
dasari venkata siddartha dinesh kumar <dinesh671@yahoo.com>
2023-01-17 18:17:02 +0530

    search page ui added

--- components/User.js
+++ components/User.js
@@ -1,17 +1,17 @@
 import { useSession, signIn,signOut } from "next-auth/react";
 import Image from "next/image";
-function User() {
+function User(className) {
   const { data: session } = useSession();
   if (session) {
     return (
       <>
-        <Image onClick={signOut} src={session.user.image} alt="" width={40} height={40} className="rounded-full hover:bg-gray-200 cursor-pointer p-1"/>
+        <Image onClick={signOut} src={session.user.image} alt="" width={40} height={40} className={`rounded-full hover:bg-gray-200 cursor-pointer p-1 ml-auto ${className}`} />
       </>
     )
   }
   return (
     <>
-      <button onClick={signIn} className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md " >Sign in</button>
+      <button onClick={signIn} className={`bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md ${className}`} >Sign in</button>
     </>
   )
 }
